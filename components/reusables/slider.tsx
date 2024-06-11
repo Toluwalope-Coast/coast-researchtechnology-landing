b57a2e0eebdbd86e51f75517af0b9d775f8724d5
generalsmart01 <michaelbrainy19@gmail.com>
2024-06-11 09:22:20 +0100

    🛠️: fixes

--- components/reusables/slider.tsx
+++ components/reusables/slider.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import { RxDotFilled } from 'react-icons/rx'; // replace 'react-icons/rx' with the actual path
-import { ISlide } from '@/utils/type';
-import { slides } from '@/utils/data';
-import Image from 'next/image';
-import styles from './component.module.css';
-import Link from 'next/link';
-import { ChevronLeftCircleIcon, ChevronRightCircleIcon } from 'lucide-react';
+import React, { useState, useEffect } from "react";
+import { RxDotFilled } from "react-icons/rx"; // replace 'react-icons/rx' with the actual path
+import { ISlide } from "@/utils/type";
+import { slides } from "@/utils/data";
+import Image from "next/image";
+import styles from "./component.module.css";
+import Link from "next/link";
+import { ChevronLeftCircleIcon, ChevronRightCircleIcon } from "lucide-react";
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -18,15 +18,27 @@ const Slider = () => {
   }, [currentSlide]);
 
   const goToPrevSlide = () => {
-    setCurrentSlide(prevSlide => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? slides.length - 1 : prevSlide - 1
+    );
   };
 
   const goToNextSlide = () => {
-    setCurrentSlide(prevSlide => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+    setCurrentSlide((prevSlide) =>
+      prevSlide === slides.length - 1 ? 0 : prevSlide + 1
+    );
   };
 
-  const DotFilled = ({ active, ...rest }) => (
-    <RxDotFilled {...rest} style={{ color: active ? ' #3c026d' : 'grey' }} />
+  interface DotFilledProps {
+    active: boolean;
+    size: number;
+  }
+
+  const DotFilled = ({ active, size, ...rest }: DotFilledProps) => (
+    <RxDotFilled
+      {...rest}
+      style={{ color: active ? "#3c026d" : "grey", fontSize: size }}
+    />
   );
 
   return (
@@ -36,29 +48,48 @@ const Slider = () => {
           <h1>{slides[currentSlide].h1}</h1>
           <p>{slides[currentSlide].p}</p>
           <button>
-            <Link href={slides[currentSlide].Link} className={styles.btn}>Read More</Link>
+            <Link href={slides[currentSlide].Link} className={styles.btn}>
+              Read More
+            </Link>
           </button>
         </div>
 
-        <Image key={slides[currentSlide].id}
+        <Image
+          key={slides[currentSlide].id}
           src={slides[currentSlide].url}
-          width={380} height={380}
+          width={380}
+          height={380}
           alt={`Slide ${currentSlide + 1}`}
-          className={`${styles.slideImage} ${slides[currentSlide].url === "3" || slides[currentSlide].url === "4" ? '' : ''}`}
+          className={`${styles.slideImage} ${
+            slides[currentSlide].url === "3" || slides[currentSlide].url === "4"
+              ? ""
+              : ""
+          }`}
         />
-
       </div>
       <div className={styles.sliderControls}>
         <button>
-          <ChevronLeftCircleIcon size={30} color='#3c026d' onClick={goToPrevSlide} />
+          <ChevronLeftCircleIcon
+            size={30}
+            color="#3c026d"
+            onClick={goToPrevSlide}
+          />
         </button>
         <div className={styles.dots}>
           {slides.map((slide: ISlide, index) => (
-            <DotFilled key={slide.id} active={index === currentSlide} size={30} />
+            <DotFilled
+              key={slide.id}
+              active={index === currentSlide}
+              size={30}
+            />
           ))}
         </div>
         <button>
-          <ChevronRightCircleIcon size={30} color='#3c026d' onClick={goToNextSlide} />
+          <ChevronRightCircleIcon
+            size={30}
+            color="#3c026d"
+            onClick={goToNextSlide}
+          />
         </button>
       </div>
     </div>
