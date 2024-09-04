4a721fbae7a82e91ce67cf58cf9cdb7f691a4a49
generalsmart01 <michaelbrainy19@gmail.com>
2024-09-04 09:31:04 +0100

    ❌: deleted unnecessary component

--- views/index.ts
+++ views/index.ts
@@ -1,6 +1,5 @@
 import AboutSection from "./aboutSection";
 import BlobApp from "./bubblesBackground";
-import HeroSliderPage from "./heroSlider";
 import ServicesSection from "./servicesSection";
 
-export { AboutSection, BlobApp, HeroSliderPage, ServicesSection };
+export { AboutSection, BlobApp, ServicesSection };
